1c612f613a26bdd4e26ac430c91b61dca87696e7
Malthe Høj-Sunesen <malthe.s@gmail.com>
2025-11-16 20:41:03 +0100

    Update SerialNumberErrorMessages.cs
    Minor updates - removed full stops and a small language correction.

--- src/AcmeCorporation.Library/SerialNumberErrorMessages.cs
+++ src/AcmeCorporation.Library/SerialNumberErrorMessages.cs
@@ -10,12 +10,12 @@
 /// formats.</remarks>
 public class SerialNumberErrorMessages
 {
-    public static string SerialNumberMustHaveContent => "Serial number must not be empty.";
-    public static string SerialNumberMustHaveLength =>$"Serial number must be {SerialNumberValues.TotalLength} long.";
-    public static string ModelNumberAllLetters => "Model number must be all letters.";
-    public static string SerialIdentifierOnlyDigits => "Serial identifier must be all digits.";
-    public static string SerialIdentifierSpecialMiddleChar => $"Serial identifier middle character must be a '{SerialNumberValues.KnownSerialIdentifierMidChar}'.";
-    public static string UniqueIdentifierWrongComposition => "Unique identifier must consist of only letters and digits.";
-    public static string UniqueIdentifierLetterPartWrong => $"The first {SerialNumberValues.UniqueIdentifierLetterPartLength} characters of the unique part must be letters.";
+    public static string SerialNumberMustHaveContent => "Serial number must not be empty";
+    public static string SerialNumberMustHaveLength =>$"Serial number must be {SerialNumberValues.TotalLength} characters long";
+    public static string ModelNumberAllLetters => "Model number must be all letters";
+    public static string SerialIdentifierOnlyDigits => "Serial identifier must be all digits";
+    public static string SerialIdentifierSpecialMiddleChar => $"Serial identifier middle character must be a '{SerialNumberValues.KnownSerialIdentifierMidChar}'";
+    public static string UniqueIdentifierWrongComposition => "Unique identifier must consist of only letters and digits";
+    public static string UniqueIdentifierLetterPartWrong => $"The first {SerialNumberValues.UniqueIdentifierLetterPartLength} characters of the unique part must be letters";
     public static string UniqueIdentifierDigitPartWrong => $"The last {SerialNumberValues.UniqueIdentifierDigitPartLength} characters of the unique part must be digits";
 }
